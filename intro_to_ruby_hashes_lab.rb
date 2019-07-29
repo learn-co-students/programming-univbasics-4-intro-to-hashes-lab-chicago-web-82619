be3c15f00213e48ebf4012de1142b79c38926ec9
Aaron Blum <blumaa@gmail.com>
2019-07-29 17:26:06 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,20 +19,18 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  another_hash = {}
-  another_hash[key] = value
+  {key => value}
   # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
-  this_hash = {}
-  this_hash[hash]
-  # binding.pry
-  return this_hash
+  return hash[key]
   # return the correct value using the hash and key parameters
 end
 
 def update_counting_hash(hash, key)
+  
+  
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
