38ef4917cb6f0bab802560fa2ec65b5922a1aa1b
Aaron Blum <blumaa@gmail.com>
2019-07-29 17:10:13 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,24 +1,34 @@
+require 'pry'
+
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash1 = {name:"Aaron", age:37}
 end
 
 def pioneer
+  wild_west = {name:'Grace Hopper'}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 def id_generator
+  you_id = {id:20}
   # return a hash with a key :id assigned to positive integer
 end
 
 def my_hash_creator(key, value)
+  another_hash = {}
+  another_hash[key] = value
   # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
+  this_hash = {}
+  this_hash[hash]
+  # binding.pry
+  return this_hash
   # return the correct value using the hash and key parameters
 end
 
